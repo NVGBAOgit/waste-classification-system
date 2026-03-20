2400edb089c267b0cfbc064290e5626f04c808fe
NVGBAOgit <giabao1607005@gmail.com>
2026-03-20 20:52:06 +0700

    Add VGG16 model implementation
    Implement VGG16 model with customizable classes and freezing options.

---
+++ SourceCode/models/vgg16.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+from torchvision import models
+
+def build_vgg16(num_classes=6, freeze=True):
+    # Load VGG16 pretrained từ ImageNet
+    model = models.vgg16(weights=models.VGG16_Weights.IMAGENET1K_V1)
+    
+    # Freeze các lớp đầu (không train lại)
+    if freeze:
+        for param in model.features.parameters():
+            param.requires_grad = False # Tắt điều chỉnh trọng số những phần đã đóng băng
+    
+    # Thay lớp cuối từ 1000 class → 6 class
+    model.classifier[6] = nn.Linear(4096, num_classes)
+    
+    return model
+
+if __name__ == "__main__":
+    model = build_vgg16(num_classes=6)
+    print(model)
+    print(f"\nSố lớp được train: {sum(p.requires_grad for p in model.parameters())}")
